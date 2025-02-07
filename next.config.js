851e702ce2ad8acf005cecb69c57af2f6a35a70a
Chaitanya Parsana <chaitanyaparsana@Chaitanyas-Mac-mini.local>
2025-02-06 19:13:54 -0800

    Add dynami deployment

--- next.config.js
+++ next.config.js
@@ -1,17 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: process.env.NODE_ENV === 'development' ? undefined : 'export',
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
-        hostname: 'localhost',
+        protocol: 'https',
+        hostname: '**',
       },
     ],
-    unoptimized: true,
   },
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
 };
 
 module.exports = nextConfig; 
